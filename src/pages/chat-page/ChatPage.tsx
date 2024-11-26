10d707147eec2f77e3c981bbeb8a1a96ab72e308
LimJaeHyun <guswogus81@gmail.com>
2024-11-26 10:51:36 +0900

    Chore : 프로젝트 초기 세팅

--- src/pages/chat-page/ChatPage.tsx
+++ src/pages/chat-page/ChatPage.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const ChatPage = () => {
   return (
     <View>
       <Text>ChatPage</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
