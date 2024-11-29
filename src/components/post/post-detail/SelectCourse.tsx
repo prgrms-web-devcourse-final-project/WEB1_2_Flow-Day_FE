27775a22291e819ad6ead6094c0c7264eb302b0e
LimJaeHyun <guswogus81@gmail.com>
2024-11-29 14:46:43 +0900

    🚀 Feat : 게시글 제목, 태그 컴포넌트

---
+++ src/components/post/post-detail/SelectCourse.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SelectCourse = () => {
+  return (
+    <View>
+      <Text>SelectCourse</Text>
+    </View>
+  );
+};
+
+export default SelectCourse;
+
+const styles = StyleSheet.create({});
