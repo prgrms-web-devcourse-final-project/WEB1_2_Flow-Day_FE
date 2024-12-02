9b511d395b06af92955f88561af832f130ab1f2d
Euiyeong <105384780+snsdl0905@users.noreply.github.com>
2024-12-02 20:09:12 +0900

    🔧 Fix : 아이폰에서 헤더가 잘리는 문제 해결

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import {Button, Text, View} from 'react-native';
+import {Button, SafeAreaView, Text, View} from 'react-native';
 import styled from 'styled-components/native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -91,108 +91,111 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarStyle: {
-            backgroundColor: '#FFFFFF',
-            height: 56,
-            borderTopWidth: 1,
-            borderTopColor: '#EEEEEE',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingBottom: 30,
-          },
-          tabBarShowLabel: true,
-          tabBarLabelPosition:'below-icon'
-        }}>
-        
-        <Tab.Screen 
-          name={ROUTES.HOME}
-          component={HomePage}
-          options={{
-            header: () => <Header>D+134</Header>,
-            tabBarIcon: ({focused}: {focused: boolean}) => {
-              return (
-                <ButtonBox>
-                  <SvgXml xml={getModifiedSvg(svg.home, focused ? COLORS.active : COLORS.inactive)} />
-                  <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>홈</ButtonText>
-                </ButtonBox>
-              );
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: '#FFFFFF',
+              height: 56,
+              borderTopWidth: 1,
+              borderTopColor: '#EEEEEE',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingBottom: 30,
             },
-          }}
-        />
+            tabBarShowLabel: true,
+            tabBarLabelPosition:'below-icon'
+          }}>
+          
+          <Tab.Screen 
+            name={ROUTES.HOME}
+            component={HomePage}
+            options={{
+              header: () => <Header>D+134</Header>,
+              tabBarIcon: ({focused}: {focused: boolean}) => {
+                return (
+                  <ButtonBox>
+                    <SvgXml xml={getModifiedSvg(svg.home, focused ? COLORS.active : COLORS.inactive)} />
+                    <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>홈</ButtonText>
+                  </ButtonBox>
+                );
+              },
+            }}
+          />
 
-        <Tab.Screen 
-          name={ROUTES.CHAT} 
-          component={ChatPage}
-          options={{ 
-            header: () => <Header>D+1234</Header>, 
-            tabBarIcon: ({ focused } : {focused: boolean}) => {
-              return (
-                <ButtonBox>
-                  <SvgXml xml={getModifiedSvg(svg.chat, focused ? COLORS.active : COLORS.inactive)} />
-                  <ButtonText style={{ color: focused ? COLORS.active : COLORS.inactive }}>
-                    채팅
-                  </ButtonText>
-                </ButtonBox>
-              )
-            }
-          }} 
-        />
+          <Tab.Screen 
+            name={ROUTES.CHAT} 
+            component={ChatPage}
+            options={{ 
+              header: () => <Header>D+1234</Header>, 
+              tabBarIcon: ({ focused } : {focused: boolean}) => {
+                return (
+                  <ButtonBox>
+                    <SvgXml xml={getModifiedSvg(svg.chat, focused ? COLORS.active : COLORS.inactive)} />
+                    <ButtonText style={{ color: focused ? COLORS.active : COLORS.inactive }}>
+                      채팅
+                    </ButtonText>
+                  </ButtonBox>
+                )
+              }
+            }} 
+          />
 
-        <Tab.Screen 
-          name={ROUTES.MAP} 
-          component={ChatPage}
-          options={{ 
-            headerShown: false,
-            tabBarIcon: ({ focused } : {focused: boolean}) => {
-              return (
-                <ButtonBox>
-                  <SvgXml xml={getModifiedSvg(svg.map, focused ? COLORS.active : COLORS.inactive)} />
-                  <ButtonText style={{ color: focused ? COLORS.active : COLORS.inactive }}>
-                    지도
-                  </ButtonText>
-                </ButtonBox>
-              )
-            }
-          }} 
-        />
+          <Tab.Screen 
+            name={ROUTES.MAP} 
+            component={ChatPage}
+            options={{ 
+              headerShown: false,
+              tabBarIcon: ({ focused } : {focused: boolean}) => {
+                return (
+                  <ButtonBox>
+                    <SvgXml xml={getModifiedSvg(svg.map, focused ? COLORS.active : COLORS.inactive)} />
+                    <ButtonText style={{ color: focused ? COLORS.active : COLORS.inactive }}>
+                      지도
+                    </ButtonText>
+                  </ButtonBox>
+                )
+              }
+            }} 
+          />
 
-        <Tab.Screen
-          name={ROUTES.POST}
-          component={ChatPage}
-          options={{
-            header: () => <Header>게시글</Header>,
-            tabBarIcon: ({focused}: {focused: boolean}) => {
-              return (
-                <ButtonBox>
-                  <SvgXml xml={getModifiedSvg(svg.post, focused ? COLORS.active : COLORS.inactive)} />
-                  <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>게시글</ButtonText>
-                </ButtonBox>
-              );
-            },
-          }}
-        />
+          <Tab.Screen
+            name={ROUTES.POST}
+            component={ChatPage}
+            options={{
+              header: () => <Header>게시글</Header>,
+              tabBarIcon: ({focused}: {focused: boolean}) => {
+                return (
+                  <ButtonBox>
+                    <SvgXml xml={getModifiedSvg(svg.post, focused ? COLORS.active : COLORS.inactive)} />
+                    <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>게시글</ButtonText>
+                  </ButtonBox>
+                );
+              },
+            }}
+          />
 
-        <Tab.Screen 
-          name={ROUTES.MY}
-          component={ChatPage}
-          options={{
-            header: () => <Header>마이페이지</Header>,
-            tabBarIcon: ({focused}: {focused: boolean}) => {
-              return (
-                <ButtonBox>
-                  <SvgXml xml={getModifiedSvg(svg.my, focused ? COLORS.active : COLORS.inactive)} />
-                  <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>마이</ButtonText>
-                </ButtonBox>
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          <Tab.Screen 
+            name={ROUTES.MY}
+            component={ChatPage}
+            options={{
+              header: () => <Header>마이페이지</Header>,
+              tabBarIcon: ({focused}: {focused: boolean}) => {
+                return (
+                  <ButtonBox>
+                    <SvgXml xml={getModifiedSvg(svg.my, focused ? COLORS.active : COLORS.inactive)} />
+                    <ButtonText style={{color: focused ? COLORS.active : COLORS.inactive}}>마이</ButtonText>
+                  </ButtonBox>
+                );
+              },
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+
   );
 };
 
