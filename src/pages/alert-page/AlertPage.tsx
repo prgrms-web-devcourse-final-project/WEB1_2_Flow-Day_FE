d17ae7205d32a1c5635e43daac66458905c22a60
LimJaeHyun <guswogus81@gmail.com>
2024-12-09 00:38:12 +0900

    pull을 위한 커밋

--- src/pages/alert-page/AlertPage.tsx
+++ src/pages/alert-page/AlertPage.tsx
@@ -1,15 +1,52 @@
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import {Alert, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
 import styled from 'styled-components/native';
 import useAlertStore from '@/store/alert/alert-store';
 import AlertItemIcon from '@/components/alert/AlertItemIcon';
 import AlertItemText from '@/components/alert/AlertItemText';
 import AlertItemDate from '@/components/alert/AlertItemDate';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+import axios from 'axios';
+import {useStore} from '@/store/useStore';
+import SockJS from 'sockjs-client';
+import {Client} from '@stomp/stompjs';
 
 const AlertPage = () => {
-  const { alerts } = useAlertStore();
+  const {alertList, addAlert, setAlertList, markAsRead} = useAlertStore();
   const navigation = useNavigation();
+  const {accessToken, userId} = useStore();
+
+  const domain = 'localhost:8090'; // 업데이트 예정!!!!!!! 업데이트 예정!!!!!!! 업데이트 예정!!!!!!!
+
+  // 알림 페이지 입장 시 -> 알림 리스트 업데이트
+  useEffect(() => {
+    const getNotifications = async () => {
+      const res = await axios.get(`http://flowday.kro.kr:80/api/v1/notifications/all`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = await res.data;
+      console.log(data);
+    };
+  }, []);
+
+  // 웹소켓 코드 =======================================
+  useEffect(() => {
+    const socket = new SockJS('http://flowday.kro.kr:80/connect'); // SockJS를 사용한 연결
+    const stompClient = new Client({
+      brokerURL: `ws://${domain}.com/ws`,
+      forceBinaryWSFrames: true,
+      appendMissingNULLonIncoming: true,
+      onConnect: () => {
+        console.log('연결됐소');
+
+        // 구독 설정
+        stompClient.subscribe(`/topic/notifications/${receiverId}`);
+      },
+    });
+  }, []);
+
   return (
     <AlertPageDesign>
       <AlertItemList>
--- src/store/alert/alert-store.ts
+++ src/store/alert/alert-store.ts
@@ -11,8 +11,13 @@ interface IAlert {
   type: 'LIKE' | 'COMMENT' | 'VOTE' | 'COUPLE' | 'COURSE'; // 수정 예정
 }
 
-const useAlertStore = create<{alerts: IAlert[]; addAlert: (alert: IAlert) => void; markAsRead: (id: number) => void}>((set) => ({
-  alerts: [
+const useAlertStore = create<{
+  alertList: IAlert[];
+  addAlert: (alert: IAlert) => void;
+  setAlertList: (alerts: IAlert[]) => void; // 새로운 배열을 받아오는 함수 추가
+  markAsRead: (id: number) => void;
+}>((set) => ({
+  alertList: [
     {
       id: 1,
       receiverId: 1,
@@ -22,29 +27,12 @@ const useAlertStore = create<{alerts: IAlert[]; addAlert: (alert: IAlert) => voi
       isRead: true,
       type: 'COMMENT',
     },
-    {
-      id: 2,
-      receiverId: 2,
-      message: '새로운 댓글이 달렸습니다!',
-      url: 'http://localhost:8080/posts/456',
-      createdAt: '2024-11-30T12:34:56',
-      isRead: false,
-      type: 'COMMENT',
-    },
-    {
-      id: 3,
-      receiverId: 3,
-      message: '새로운 댓글이 달렸습니다!',
-      url: 'http://localhost:8080/posts/789',
-      createdAt: '2024-12-29T12:34:56',
-      isRead: false,
-      type: 'COMMENT',
-    },
   ],
-  addAlert: (alert) => set((state) => ({alerts: [...state.alerts, alert]})),
+  addAlert: (alert) => set((state) => ({alertList: [...state.alertList, alert]})),
+  setAlertList: (alerts) => set({alertList: alerts}), // 새로운 배열을 상태로 설정
   markAsRead: (id) =>
     set((state) => ({
-      alerts: state.alerts.map((alert) => (alert.id === id ? {...alert, isRead: true} : alert)),
+      alertList: state.alertList.map((alert) => (alert.id === id ? {...alert, isRead: true} : alert)),
     })),
 }));
 
